79bb9741a36e48ec185a1ce7171f01fc73238a0f
eiuthanghuynh <thang.huynhle.cit22@eiu.edu.vn>
2026-03-31 15:42:52 +0700

    Fix home không hiện thông tin món & cart

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -68,24 +68,3 @@ function loadBestSellersToHome() {
 function formatPrice(price) {
   return price.toLocaleString("vi-VN", { style: "currency", currency: "VND" });
 }
-
-let linkIntroductionSliders = [
-  "./assets/combo/combo-mung-khai-truong.png",
-  "./assets/combo/combo-buoi-trua-vui-ve.png",
-  "./assets/combo/combo-sieu-tiet-kiem.png",
-  "./assets/combo/burger.png",
-  "./assets/combo/burrito.png",
-]
-
-function printIntroductionSlider() {
-  let introArr = ``;
-  let i = 1;
-  for (const link of linkIntroductionSliders) {
-    introArr +=
-      `<div class="carousel-item ${i === 1 ? "active" : ""}">
-                <a href=""><img src="${link}" class="d-block w-100" alt="Slider Image"></a>
-              </div>`;
-    i++;
-  }
-  $("#intro").html(introArr);
-}
